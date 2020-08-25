d802313784742b5c6b3908091a4a11a23ceb636f
Juan Carlos Peña Peña <jcpena@stefanini.com>
2020-08-25 08:58:42 -0500

    Binding properties from app component to server element component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{
+    name: 'Testserver',
+    type: 'server',
+    content: 'Just a server'
+  }];
 
 
 
