cd41ef302b234b34795f3b9c96726792e3d4b273
Juan Carlos Peña Peña <penapena@uninorte.edu.co>
2020-08-27 22:02:03 -0500

    lifecycle with ngDoCheck, AfterContentInit, AfterContentChecked, AfterViewInit and AfterViewChecked

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
 
   @Input() element: {name: string, type: string, content: string};
   @Input() name;
 
   constructor() { 
     console.log('Constructor Called');
-  }
+  }  
 
   ngOnChanges(changes: SimpleChanges) {
     console.log('ngOnChanges called');
@@ -21,6 +21,26 @@ export class ServerElementComponent implements OnInit, OnChanges {
 
   ngOnInit(): void {
     console.log('ngOnInit called');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called');
+  }
+  
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
   }
 
 }
