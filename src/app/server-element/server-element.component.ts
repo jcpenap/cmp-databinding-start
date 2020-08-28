31846b1208c2ab1e1a82d01fa7ce5e2042ff6a92
Juan Carlos Peña Peña <penapena@uninorte.edu.co>
2020-08-27 22:08:44 -0500

    lifecycle with OnDestroy

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   @Input() element: {name: string, type: string, content: string};
   @Input() name;
@@ -38,9 +38,13 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   ngAfterViewChecked(): void {
     console.log('ngAfterViewChecked called');
   }
-  
+
   ngAfterViewInit(): void {
     console.log('ngAfterViewInit called');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
   }
 
 }
