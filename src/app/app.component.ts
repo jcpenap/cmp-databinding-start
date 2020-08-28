d327608c9ffd897ac56a907a521b21bad7448b29
Juan Carlos Peña Peña <penapena@uninorte.edu.co>
2020-08-27 21:42:53 -0500

    lifecycle with ngOnChanges and ngOnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,4 +28,8 @@ export class AppComponent {
     });
   }
 
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed';
+  }
+
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
 
   @Input() element: {name: string, type: string, content: string};
+  @Input() name;
 
-  constructor() { }
+  constructor() { 
+    console.log('Constructor Called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
   }
 
 }
